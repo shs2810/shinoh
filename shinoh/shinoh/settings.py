97cfeacedec27465afc415757c87e0e118b43d14
dennyoh <dennyoh9211@gmail.com>
2017-08-16 14:33:53 +0900

    css 수정

--- shinoh/shinoh/settings.py
+++ shinoh/shinoh/settings.py
@@ -114,9 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
